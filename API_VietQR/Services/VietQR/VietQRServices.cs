06c2321f8683cd93720565da1a74ec5e2b97e08b
DINH NGOCDUONG <duongdn@vnisc.com.vn>
2024-03-12 09:59:58 +0700

    Update

--- API_VietQR/Services/VietQR/VietQRServices.cs
+++ API_VietQR/Services/VietQR/VietQRServices.cs
@@ -7,6 +7,7 @@ using API_VietQR.Utilities;
 using Dapper;
 using Dapper.Contrib.Extensions;
 using Newtonsoft.Json;
+using Pipelines.Sockets.Unofficial.Arenas;
 
 namespace API_VietQR.Services.VietQR
 {
@@ -26,7 +27,7 @@ namespace API_VietQR.Services.VietQR
 		}
         public async Task<TransactionSyncReponse> TransactionSync(int agentId, TransactionSyncRequest request, CancellationToken cancellationToken)
         {
-			string fullPath = AppDomain.CurrentDomain.BaseDirectory + "/LogRequest/Tran_" + Guid.NewGuid() + ".txt";
+			//string fullPath = AppDomain.CurrentDomain.BaseDirectory + "/LogRequest/Tran_" + Guid.NewGuid() + ".txt";
 			//using (StreamWriter writer = new StreamWriter(fullPath))
 			//{
 			//	//writer.WriteLine(JsonConvert.SerializeObject(request));
@@ -81,7 +82,28 @@ namespace API_VietQR.Services.VietQR
 									SubAgentID = Convert.ToInt64(TmpSubAgentID);
 									sSQL = "select * from tbl_SubAgent where ID=@ID and ParentAgent=@AgentID";
 								}
-							}							
+							}
+							if (String.IsNullOrEmpty(sSQL))
+							{
+								var TmpSubAgentID = "";
+								char[] charArr = TmpSubAgent.ToCharArray();
+								foreach (var item in charArr)
+								{
+									if (Utilities.ValidationUtility.IsNumeric(item.ToString()))
+									{
+										TmpSubAgentID += item.ToString();
+									}
+									else
+									{
+										break;
+									}
+								}
+								if (Utilities.ValidationUtility.IsNumeric(TmpSubAgentID))
+								{
+									SubAgentID = Convert.ToInt64(TmpSubAgentID);
+									sSQL = "select * from tbl_SubAgent where ID=@ID and ParentAgent=@AgentID";
+								}
+							}
 						}
 						else
 						{
@@ -104,46 +126,50 @@ namespace API_VietQR.Services.VietQR
 									sSQL = "select * from tbl_SubAgent where AgentCode=@SubAgentCode and ParentAgent=@AgentID";
 								}
 							}
-						}	
-						
-						var objSubAgent = dbBooking.QueryFirstOrDefault<SubAgents>(sSQL,
-						new { AgentID = agentId, ID = SubAgentID, SubAgentCode = SubAgentCode });
-						if (objSubAgent != null)
-						{
-							var objAgentCallBackCheck = db.QueryFirstOrDefault<Agent_VietQR_CallBack>("select * from tbl_Agent_VietQR_CallBack where ReferenceNumber=@ReferenceNumber", new { ReferenceNumber = objAgentCallBack.ReferenceNumber });
-							if (objAgentCallBackCheck == null)
-							{
-								var DocType = "T";
-								var objDocNo = dbBooking.QueryFirstOrDefault<string>("Select [dbo].[GET_DOC_NO](@DocType,@AgentID,@SubAgentID)",
-									new { DocType = DocType, AgentID = agentId, SubAgentID = objSubAgent.ID });
-								if (!String.IsNullOrEmpty(objDocNo))
-								{
-									decimal Fee = 0;
-									if (request.amount < 10000000)
-									{
-										Fee = objAgentVietQR.Fee;
-									}
-									CN_NhatKys objNhatKy = new CN_NhatKys();
-									objNhatKy.AgentID = agentId;
-									objNhatKy.Amount_NT = request.amount - Fee;
-									objNhatKy.Amount_VND = request.amount - Fee;
-									objNhatKy.Doc_Date = DateTime.Now;
-									objNhatKy.Doc_No = objDocNo;
-									objNhatKy.Doc_Type = DocType;
-									objNhatKy.Doc_Title = objDocNo;
-									objNhatKy.MemberID = objThanhVien.ID;
-									objNhatKy.ROE = 1;
-									objNhatKy.SubAgent = objSubAgent.ID;
-									objNhatKy.OtherFee = 0;
-									objNhatKy.AccType = "VN-BL-VJ";
-									dbBooking.Insert<CN_NhatKys>(objNhatKy);
-									objAgentCallBack.Status = 1;
-
-								}
-							}
-							
-
 						}
+						
+						if (!String.IsNullOrEmpty(sSQL))
+						{
+							var objSubAgent = dbBooking.QueryFirstOrDefault<SubAgents>(sSQL,
+						new { AgentID = agentId, ID = SubAgentID, SubAgentCode = SubAgentCode });
+							if (objSubAgent != null)
+							{
+								var objAgentCallBackCheck = db.QueryFirstOrDefault<Agent_VietQR_CallBack>("select * from tbl_Agent_VietQR_CallBack where ReferenceNumber=@ReferenceNumber", new { ReferenceNumber = objAgentCallBack.ReferenceNumber });
+								if (objAgentCallBackCheck == null)
+								{
+									var DocType = "T";
+									var objDocNo = dbBooking.QueryFirstOrDefault<string>("Select [dbo].[GET_DOC_NO](@DocType,@AgentID,@SubAgentID)",
+										new { DocType = DocType, AgentID = agentId, SubAgentID = objSubAgent.ID });
+									if (!String.IsNullOrEmpty(objDocNo))
+									{
+										decimal Fee = 0;
+										if (request.amount < 10000000)
+										{
+											Fee = objAgentVietQR.Fee;
+										}
+										CN_NhatKys objNhatKy = new CN_NhatKys();
+										objNhatKy.AgentID = agentId;
+										objNhatKy.Amount_NT = request.amount - Fee;
+										objNhatKy.Amount_VND = request.amount - Fee;
+										objNhatKy.Doc_Date = DateTime.Now;
+										objNhatKy.Doc_No = objDocNo;
+										objNhatKy.Doc_Type = DocType;
+										objNhatKy.Doc_Title = objDocNo;
+										objNhatKy.MemberID = objThanhVien.ID;
+										objNhatKy.ROE = 1;
+										objNhatKy.SubAgent = objSubAgent.ID;
+										objNhatKy.OtherFee = 0;
+										objNhatKy.AccType = "VN-BL-VJ";
+										dbBooking.Insert<CN_NhatKys>(objNhatKy);
+										objAgentCallBack.Status = 1;
+
+									}
+								}
+
+
+							}
+						}
+						
 						
 					}	
 					
